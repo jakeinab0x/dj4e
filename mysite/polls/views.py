f140c86ac4f4db1d71b5d0e6a65d259febf3afa9
jakeinab0x <98402073+jakeinab0x@users.noreply.github.com>
2023-05-20 23:19:05 +0100

    commit

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,4 +1,4 @@
 from django.http import HttpResponse
 
 def index(Request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    return HttpResponse("Hello, world. 11808b3e is the polls index.")
