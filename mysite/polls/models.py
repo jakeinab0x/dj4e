d3688d7c6b5e01c4f50843b439410a31fcd2007d
jakeinab0x <98402073+jakeinab0x@users.noreply.github.com>
2023-05-23 22:11:45 +0100

    commit

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -5,3 +5,18 @@ from django.db import models
 class User(models.Model):
     name = models.CharField(max_length=128)
     email = models.CharField(max_length=128)
+
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+
+    def __str__(self):
+        return self.question_text
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.choice_text
