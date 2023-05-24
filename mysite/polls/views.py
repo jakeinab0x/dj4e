28774dbe43bb645a301af8351a191e077039bf4c
jakeinab0x <98402073+jakeinab0x@users.noreply.github.com>
2023-05-24 23:10:33 +0100

    commit

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Question, Choice
 
 
@@ -7,16 +7,15 @@ def index(request):
     latest_question_list = Question.objects.order_by("-pub_date")[:5]
     context = {"latest_question_list": latest_question_list}
     return render(request, "polls/index.html", context)
-    # return HttpResponse(f"Hello, world. 11808b3e is the polls index. The Answer is {answer}.")
 
 def owner(request):
     return HttpResponse("Hello, world. 11808b3e is the polls index.")
 
 def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
     correct_answer = str(Choice.objects.get(choice_text="42"))
-    context = {"correct_answer": correct_answer}
+    context = {"question": question, "correct_answer": correct_answer}
     return render(request, "polls/detail.html", context)
-    # return HttpResponse(f"You're looking at question {question_id}")
 
 def results(request, question_id):
     return HttpResponse(f"You're looking at the results of question {question_id}")
