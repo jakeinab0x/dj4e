70e2810290a13cd3f105248b8ffe3ecd8995ca5f
jakeinab0x <98402073+jakeinab0x@users.noreply.github.com>
2024-06-26 22:49:07 +0100

    commit

--- mysite/autos/views.py
+++ mysite/autos/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.shortcuts import render, redirect, get_object_or_404
+from django.views import View
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
 
-# Create your views here.
+from autos.models import Auto, Make
+from autos.forms import MakeForm
+
--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -28,6 +28,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('polls/', include('polls.urls')), 
     path('hello/', include('hello.urls')), 
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('autos/', include('autos.urls')),
     path('', TemplateView.as_view(template_name='home/main.html')),
     re_path(r'^site/(?P<path>.*)$', serve,
         {'document_root': SITE_ROOT, 'show_indexes': True},
