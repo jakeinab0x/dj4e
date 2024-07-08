6382ce7d4e53c2ed044eeb45c2d95cffbc45ed6c
jakeinab0x <98402073+jakeinab0x@users.noreply.github.com>
2024-07-08 19:53:48 +0100

    add Cats generic edit views

--- mysite/cats/views.py
+++ mysite/cats/views.py
@@ -1,3 +1,54 @@
-from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.shortcuts import render, redirect, get_object_or_404
+from django.views import View
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+
+from cats.models import Cat, Breed
 
 # Create your views here.
+
+class MainView(LoginRequiredMixin, View):
+    """Main list view for Cats app with count"""
+    def get(self, request):
+        bc = Breed.objects.count()
+        cl = Cat.objects.all()
+
+        ctx = {'breed_count': bc, 'cat_list': cl}
+        return render(request, 'cats/cat_list.html', ctx)
+    
+
+class BreedCreate(LoginRequiredMixin, CreateView):
+    model = Breed
+    fields = '__all__'
+    success_url = reverse_lazy('cats:all')
+
+
+class BreedUpdate(LoginRequiredMixin, UpdateView):
+    model = Breed
+    fields = '__all__'
+    success_url = reverse_lazy('cats:all')
+
+
+class BreedDelete(LoginRequiredMixin, DeleteView):
+    model = Breed
+    fields = '__all__'
+    success_url = reverse_lazy('cats:all')
+
+
+class CatCreate(LoginRequiredMixin, CreateView):
+    model = Cat
+    fields = '__all__'
+    success_url = reverse_lazy('cats:all')
+
+
+class CatUpdate(LoginRequiredMixin, UpdateView):
+    model = Cat
+    fields = '__all__'
+    success_url = reverse_lazy('cats:all')
+
+
+class CatDelete(LoginRequiredMixin, DeleteView):
+    model = Cat
+    fields = '__all__'
+    success_url = reverse_lazy('cats:all')
