d5d58df79bb337c2f51eb9809c1c984e44789eec
jakeinab0x <98402073+jakeinab0x@users.noreply.github.com>
2024-11-22 16:20:22 +0000

    register models on admin

--- mysite/ads/admin.py
+++ mysite/ads/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from ads.models import Ad
+from ads.models import Ad, Comment, Fav
 
 # Register your models here.
-admin.site.register(Ad)
+admin.site.register([Ad, Comment, Fav])
