e8193d01171047682264547a9f7d71af6cb73ef5
jakeinab0x <98402073+jakeinab0x@users.noreply.github.com>
2024-11-24 13:01:20 +0000

    update view and template

--- mysite/solo/views.py
+++ mysite/solo/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from django.urls import reverse
+from django.views.generic import TemplateView
 
-# Create your views here.
+class FormView(TemplateView):
+    template_name = 'solo/form.html'
+
+    def get(self, request):
+        result = request.session.get('result', False)
+        if result: del(request.session['result'])
+        return render(request, self.template_name, {'result': result})
+    
+    def post(self, request):
+        if request.POST.get('field1') or request.POST.get('field2'):
+            rev_field1 = request.POST.get('field1').strip().upper()[::-1]
+            rev_field2 = request.POST.get('field2').strip().upper()[::-1]
+            result = f'{rev_field2} {rev_field1}'
+            request.session['result'] = result
+        return redirect(request.path)
